304bf02f127e3772f9532995e0f662666609f83d
jason1223456 <102353552+jason1223456@users.noreply.github.com>
2025-05-05 16:34:24 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // 讓 Tailwind 掃描這些檔案
+      "./public/index.html"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
