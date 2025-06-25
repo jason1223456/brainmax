65d2b6579eefd1bc2e76dc1a95492744bb019fde
jason1223456 <102353552+jason1223456@users.noreply.github.com>
2025-06-25 19:53:37 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from "./LoginPage";
 import HomePage from "./HomePage";
 import NewPage from "./NewPage"; // 新增引入 NewPage
 import Reads from "./Reads"; 
+import AiReport from "./AiReport"; 
 export default function App() {
   return (
     <Router>
@@ -10,10 +11,12 @@ export default function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/newpage" element={<NewPage />} />
-        <Route path="/Reads" element={<Reads/>} /> {/* 新增這行 */}
+        <Route path="/Reads" element={<Reads/>} /> 
+        <Route path="/AiReport" element={<AiReport/>} /> 
       </Routes>
     </Router>
   );
 }
+
 
 
